c3cae3ad6bda5797a0a1375d18064477d4f271bd
Mehwish39 <124083054+Mehwish39@users.noreply.github.com>
2023-02-21 10:08:11 -0800

    Update posts.js

--- controllers/posts.js
+++ controllers/posts.js
@@ -27,6 +27,79 @@ exports.addPost = async function(req, res, next) {
     }
 };
 
+
+exports.readPost = async function (req, res, next) {
+    
+    try {
+        const userId = await req.params.userId;
+        const postsData = await Post.find({ userId: userId });
+      
+        if (postsData == null) {
+            res.status(404).send("No Data Found");
+        } else {
+            res.json(postsData);
+        }
+
+    } catch (err) {
+        return res.status(400).json({
+            success: false,
+            error: err
+        });
+    }
+};
+
+
+
+exports.readByIdPost = async function (req, res, next) {
+
+    try {
+        const userId = await req.params.userId;
+        const postId = await req.params.postId;
+        const postData = await Post.findOne({ userId: userId, _id: postId });
+
+        if (!postData) {
+            res.status(404).send("Post Not Found");
+        } else {
+            res.json(postData);
+        }
+
+    } catch (err) {
+        return res.status(400).json({
+            success: false,
+            error: err
+        });
+    }
+};
+
+
+
+exports.updatePost = async function (req, res, next) {
+
+    try {
+        const postId = await req.params.postId;
+        const userId = await req.params.userId;
+        const newUpdatedAt = new Date();
+        const updatedPost = await Post.findOneAndUpdate(
+            { _id: postId, userId: userId },
+            { $set: { updatedAt: newUpdatedAt, ...req.body } },
+            { new: true }
+        );
+
+        if (!updatedPost) {
+            res.status(404).send("Post Not Found");
+        } else {
+            res.json(updatedPost);
+        }
+
+    } catch (err) {
+        return res.status(400).json({
+            success: false,
+            error: err
+        });
+    }
+};
+
+
 exports.deletePost = async function(req, res, next) {
     try {
 
