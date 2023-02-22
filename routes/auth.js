100e22a2cc77936b72c1782784d6e9de2292d3af
Deeba Fahad <deebanasir@gmail.com>
2023-02-22 21:48:35 +0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -15,13 +15,6 @@ router.post('/signup', authController.signup);
 router.post('/forgot-password', authController.forgotPassword);
 router.post('/reset-password', authController.resetPassword);
 
-
-// Add a post 
-router.post('/users/:userId/posts', postController.addPost);
-
-// Delete a post by a user with a particular post id
-router.delete('/users/:userId/posts/:postId', postController.deletePost);
-
 router.patch('/users/:userId/posts/:postId', postController.updatePost);
 router.get('/users/:userId/posts', postController.readPost);
 router.get('/users/:userId/posts/:postId', postController.readByIdPost);
