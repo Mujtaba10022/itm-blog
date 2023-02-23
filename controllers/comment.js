168ef8cfb3c5575e105bfae193afdab70345a0db
Masuma09 <125720290+Masuma09@users.noreply.github.com>
2023-02-23 08:28:01 +0500

    validations on update

--- controllers/comment.js
+++ controllers/comment.js
@@ -1,86 +1,35 @@
-const Comment = require("../models/Comments");
 const Posts = require("../models/Post");
+const Comment = require("../models/Comments");
+
 
 module.exports = {
-  create: function (req, res) {
+  create: async function (req, res) {
     // find out which post you are commenting
-
-<<<<<<< HEAD
-    create: async function (req, res){
-    
-        // find out which post you are commenting
-        
-        const id = req.params.id;
-        console.log(id);
-        // get the comment text and record post id
-        const blogPost =  await Posts.findById(id);
-      
-        console.log(blogPost);
-        
-        const comment = new Comment({
-         text: req.body.comment,
-         post_id: id
-      })
-=======
     const postId = req.params.postId;
     // get the comment text and record post id
     Posts.findById(postId, async function (err, blogPost) {
       console.log(blogPost);
+      
       if (err) {
         return res.status(400).json({
           success: false,
-          error: err,
-        });
-      } else {
+          
+          });
+
+        }      
+        else {
         const comment = new Comment({
           text: req.body.comment,
           post: postId,
         });
->>>>>>> 3c7bb51d34f183c395e75de998952a79ac63fee3
         // save comment
         await comment.save();
-        // push the comment into the blogPost.comments array
-
-<<<<<<< HEAD
-      await blogPost.save(function(err) {
-      if(err) {console.log(err)}
-      res.status(200).send("comment added")
-      })
-      
-      },
-
-
-      // delete Controller
-      delete: function(req,res){
-
-        const comment_id = req.params.comment_id;
-        console.log(comment_id);
-        res.send("good");
-=======
-        blogPost.comments.push(comment);
-
-        // save and send status
-
-        await blogPost.save(function (err) {
-          if (err) {
-            console.log(err);
-          }
-        });
-        return res.status(202).json({
-          success: true,
-          data: {
-            comment: comment,
-            blogPost: blogPost,
-          },
-        });
->>>>>>> 3c7bb51d34f183c395e75de998952a79ac63fee3
       }
     });
   },
 
   deleteComment: function (req, res) {
     const commentId = req.body.commentId;
-
     Comment.findByIdAndDelete(commentId, function (err) {
       if (err) {
         return res.status(400).json({
@@ -96,10 +45,35 @@ module.exports = {
     });
   },
 
-  updateComment: function (req, res) {
-    const commentId = req.body.commentId;
 
+  updateComment: async function (req, res) {
+    const commentId = req.body.commentId;
     const newComment = req.body.comment;
+
+    let checkPost = await Comment.find({commentId}).populate('post_id');
+
+    if(checkPost == null || !checkPost) {
+      return res.status(400).json({
+          success: false,
+          error: validationError([{
+              fieldName: 'post_id', 
+              errorType: 'Not Found', 
+              errorMessage: 'Posts not found.'
+          }])
+      })
+  }
+
+  let checkComment = await Comment.findOne({commentId});
+  if(!checkComment){
+    return res.status(400).json({
+      success: false,
+      error: validationError([{
+          fieldName: 'comment_id', 
+          errorType: 'Not Found', 
+          errorMessage: 'Comment not found.'
+      }])
+  })
+  }
 
     Comment.findByIdAndUpdate(
       commentId,
@@ -108,7 +82,11 @@ module.exports = {
         if (err) {
           return res.status(400).json({
             success: false,
-            error: err,
+            error: validationError([{
+              fieldName: 'comment_id', 
+              errorType: 'not updated', 
+              errorMessage: 'Can not be updated.'
+          }])
           });
         } else {
           return res.status(202).json({
@@ -121,7 +99,6 @@ module.exports = {
       }
     );
   },
-
   showComments: async function (req, res) {
     try {
       const allComments = await Comment.find({});
@@ -138,4 +115,5 @@ module.exports = {
       });
     }
   },
-};
+
+}
--- controllers/posts.js
+++ controllers/posts.js
@@ -8,20 +8,6 @@ module.exports = {
     });
     //     // save post
 
-<<<<<<< HEAD
-    create: async function (req, res) {
-     console.log(req.body);
-        const blogPost = new Posts({
-         title: req.body.title,
-          text: req.body.text
-      })
-        // save post
-      await blogPost.save();
-      res.send("your post has been added");
-    }
-      
-}
-=======
     blogPost.save(function (err, blogPost) {
       if (err) {
         return res.status(400).json({
@@ -39,4 +25,3 @@ module.exports = {
     });
   },
 };
->>>>>>> 3c7bb51d34f183c395e75de998952a79ac63fee3
